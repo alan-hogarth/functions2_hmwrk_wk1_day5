f91bc86323d7bcbbe3d8ffda32c40d0739208c9b
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-07 13:56:38 +0000

    my first commit

---
+++ src/pet_shop.py
@@ -0,0 +1,14 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(title):
+    name_result = None
+    for shop_name in title.values():   
+        name_result = shop_name
+    
+    return name_result
+    
+    
+    
+    
+    
+
